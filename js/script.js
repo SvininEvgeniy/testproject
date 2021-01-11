c97b2a547bc94e75c55943a4f00c5061b0c7bd7f
Evgeniy Svinin <prooflist93@gmail.com>
2021-01-11 22:08:55 +0300

    second practice ver.2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
-/* 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-if (numberOfFilms ==0) {
-   alert('Произошла ошибка');
-} else if (numberOfFilms <= 10) {
-   alert('Просмотрено довольно мало фильмов');
-} else if (numberOfFilms > 10 && numberOfFilms <= 30){
-   alert('Вы классический зритель');
-} else { 
-   alert('Вы киноман');
-};
 
 let personalMovieDB = {
    count: numberOfFilms,
@@ -35,5 +20,15 @@ for (let i = 1; i <= 2; i++){
       i--;
    }               
 };
+
+if (personalMovieDB.count < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
