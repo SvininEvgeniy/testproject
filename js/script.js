be69ae04f4048d6003b28844723d7cdd418f83bf
Evgeniy Svinin <prooflist93@gmail.com>
2021-01-15 22:01:14 +0300

    fourth practice

--- js/script.js
+++ js/script.js
@@ -1,68 +1,68 @@
 'use strict';
-let numberOfFilms;
-
-function start() {
-   numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-      numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-   }
-};
-
-start();
 
 let personalMovieDB = {
-   count: numberOfFilms,
+   count: 0,
    movies: {},
    actors: {},
    genres: [],
-   privat : false
-};
-
-function rememberMyFilms() {
-   for (let i = 1; i <= 2; i++){
-      let a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');   
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done'); 
+   privat : false,
+   start: function() {
+               personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+               while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+               personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+               }
+   },
+   rememberMyFilms: function() {
+                        for (let i = 1; i <= 2; i++){
+                           let a = prompt('Один из последних просмотренных фильмов?', ''),
+                              b = prompt('На сколько оцените его?', '');   
+                           if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                           personalMovieDB.movies[a] = b;
+                           console.log('done'); 
+                           } else {
+                              console.log('error');
+                              i--;
+                           }               
+                        }; 
+   },
+   detectPersonalLevel: function() {
+      if (personalMovieDB.count < 10) {
+         console.log("Просмотрено довольно мало фильмов");
+      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+         console.log("Вы классический зритель");
+      } else if (personalMovieDB.count >= 30) {
+         console.log("Вы киноман");
       } else {
-         console.log('error');
-         i--;
-      }               
-   }; 
-}
+         console.log("Произошла ошибка");
+      };
+   },
+   showMyDB: function(hidden) {
+      if (!hidden) {
+         console.log(personalMovieDB);
+      } 
+   },
+   toggleVisibleMyDB: function() {
+      if (personalMovieDB.privat) {
+         personalMovieDB.privat = false;
+      } else {
+         personalMovieDB.privat = true;
+      }
+   },
+   writeYourGenres: function() {
+      for (let i = 1; i <= 3; i++) {
+         let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-   if (personalMovieDB.count < 10) {
-      console.log("Просмотрено довольно мало фильмов");
-   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-      console.log("Вы классический зритель");
-   } else if (personalMovieDB.count >= 30) {
-      console.log("Вы киноман");
-   } else {
-      console.log("Произошла ошибка");
-   };
+         if (genre === '' || genre === null) {
+            console.log('Вы ввели некорректные данные или пустую строку');
+            i--; 
+         } else {
+            personalMovieDB.genres[i - 1] = genre;
+         };
+         
+         personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i+1} - это ${item}`);
+         });
+         
+      };
+   }
 };
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB);
-   } 
-}
-
-showMyDB(personalMovieDB.privat);
-
-let numberOfGenres;
-
-function writeYourGenres() {
-   for (let i = 1; i <= 3; i++) {
-      personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-   };
-}
-
-writeYourGenres();
